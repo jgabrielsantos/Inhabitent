34741b9a643170a03575646e3fb462c1e2a35fa1
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-02-12 20:50:21 -0800

    added widgets to footer

--- plugins/Contact_Info/views/widget.php
+++ plugins/Contact_Info/views/widget.php
@@ -1,42 +1,55 @@
 <!-- This file is used to markup the public-facing widget. -->
 <p>
     <?php if (strlen(trim($phone)) > 0) : ?>
-        <span class="phone"><i class="fas fa-phone-alt"></i></span>
-        <a href=""><?php echo $phone; ?></a>
+        <span class="contact phone">
+            <i class="fas fa-phone-alt"></i>
+            <a href=""><?php echo $phone; ?></a>
+        </span>
     <?php endif; ?>
 </p>
 
 <p>
     <?php if (strlen(trim($email)) > 0) : ?>
-        <span class="email"><i class="fas fa-envelope"></i></span>
-        <a href=""><?php echo $email; ?></a>
+        <span class="contact email">
+            <i class="fas fa-envelope"></i>
+            <a href=""><?php echo $email; ?></a>
+        </span>
     <?php endif; ?>
 </p>
+<div class="social-media-icons">
+    <p>
+        <?php if (strlen(trim($facebook)) > 0) : ?>
+            <span class="facebook">
+                <i class="fab fa-facebook-square"></i>
+                <a href="<?php echo $facebook; ?>"></a>
+            </span>
+        <?php endif; ?>
+    </p>
 
-<p>
-    <?php if (strlen(trim($facebook)) > 0) : ?>
-        <span class="facebook"><i class="fab fa-facebook-square"></i></span>
-        <a href=""><?php echo $facebook; ?></a>
-    <?php endif; ?>
-</p>
+    <p>
+        <?php if (strlen(trim($twitter)) > 0) : ?>
+            <span class="twitter">
+                <i class="fab fa-twitter-square"></i>
+                <a href="<?php echo $twitter; ?>"></a>
+            </span>
+        <?php endif; ?>
+    </p>
 
- <p>
-    <?php if (strlen(trim($twitter)) > 0) : ?>
-        <span class="twitter"><i class="fab fa-twitter-square"></i></span>
-        <a href=""><?php echo $twitter; ?></a>
-    <?php endif; ?>
-</p>
-
-<p>
-    <?php if (strlen(trim($google)) > 0) : ?>
-        <span class="google"><i class="fab fa-google-plus-square"></i></span>
-        <a href=""><?php echo $google; ?></a>
-    <?php endif; ?>
-</p>
+    <p>
+        <?php if (strlen(trim($google)) > 0) : ?>
+            <span class="google">
+                <i class="fab fa-google-plus-square"></i>
+                <a href="<?php echo $google; ?>"></a>
+            </span>
+        <?php endif; ?>
+    </p>
+</div>
 
 <p>
     <?php if (strlen(trim($location)) > 0) : ?>
-        <span class="location"><i class="fas fa-map-marker-alt"></i></span>
-        <p><?php echo $location; ?></p>
+        <span class="location">
+            <i class="fas fa-map-marker-alt"></i>
+            <p><?php echo $location; ?></p>
+        </span>
     <?php endif; ?>
 </p>
--- themes/Inhabitent/Inhabitent-starter/footer.php
+++ themes/Inhabitent/Inhabitent-starter/footer.php
@@ -10,8 +10,16 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div id="secondary" class="widget-area" role="complementary">
-					<?php dynamic_sidebar( 'footer_sidebar' ); ?>
+				<div class="first-grid-row">
+					<div id="secondary" class="widget-area" role="complementary">
+						<?php dynamic_sidebar( 'footer_sidebar' ); ?>
+					</div>
+					<div class="footer-logo">
+						<img src="<?php echo get_stylesheet_uri() ?>/project-04/images/logos/inhabitent-logo-tent.svg" alt="Footer Logo">
+					</div>
+				</div>
+				<div class="site-info">
+					<p>COPYRIGHT &#169 2019 INHABITENT</p>
 				</div>
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
