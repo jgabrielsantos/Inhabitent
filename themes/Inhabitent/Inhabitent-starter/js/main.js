cb0d2eab40c046b589656c0d99ac7454a9b46fa7
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-02-13 20:17:06 -0800

    delete console.logs from function

--- themes/Inhabitent/Inhabitent-starter/js/main.js
+++ themes/Inhabitent/Inhabitent-starter/js/main.js
@@ -1,7 +1,5 @@
 (function() {
     let searchIcon = document.getElementById('search-toggle');
-    console.log(searchIcon);
-
     searchIcon.onclick = function(event) {
         event.preventDefault();
         let searchForm = document.getElementById('search-form');
@@ -9,7 +7,6 @@
             searchForm.classList.remove("hide-search");
             searchForm.classList.add("display-search");
         } else {
-            console.log('teste22');
             searchForm.classList.remove("display-search");
             searchForm.classList.add("hide-search");
         }
