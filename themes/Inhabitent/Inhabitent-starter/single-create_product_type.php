6b7dc9b79b7e22d11d01bf38bc2afbe5020352f6
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2020-02-18 15:16:22 -0800

    merge scss for homepage and about page, changed style for single product page, changed code for single products and it's page

--- themes/Inhabitent/Inhabitent-starter/single-create_product_type.php
+++ themes/Inhabitent/Inhabitent-starter/single-create_product_type.php
@@ -10,19 +10,15 @@ get_header(); ?>
 	<div id="primary" class="content-area container">
 		<main id="main" class="site-main" role="main">
 
-		<?php while ( have_posts() ) : the_post(); ?>
+			<?php while ( have_posts() ) : the_post(); ?>
 		
-			<?php
-				get_template_part( 'template-parts/content' );
-			?>
+				<?php
+					get_template_part( 'template-parts/content-single' );
+				?>
 
-		<?php endwhile; // End of the loop. ?>
-			<?php the_posts_navigation(); ?>
-			
-		</article>
+			<?php endwhile; // End of the loop. ?>
 			
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
--- themes/Inhabitent/Inhabitent-starter/template-parts/content-single.php
+++ themes/Inhabitent/Inhabitent-starter/template-parts/content-single.php
@@ -8,29 +8,20 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() ) : ?>
-			<?php the_post_thumbnail( 'large' ); ?>
-		<?php endif; ?>
+    <div class="single-product-image-wrapper">
+		<img src="<?php echo CFS()->get( 'Image' ); ?>" class="single-product-image"> 
+	</div>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+    <div class="single-product-content-wrapper">
+        <header class="entry-header">
+            <h1 class="entry-title"><?php the_title(); ?></h1> </header>
+        <!-- .entry-header -->
 
-		<div class="entry-meta">
-			<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+        <div class="entry-content">
+            <p class="price"><?php echo CFS()->get('Price') ?></p>
 
-	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php red_starter_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
+            <p><?php the_content();?></p>
+        </div>
+        <!-- .entry-content -->
+    </div>
+</article>
